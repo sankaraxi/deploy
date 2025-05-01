fe3ca618fe87275931238cbb17e7f1076f5ee2c5
Sankar K G <sankarfrompalani@gmail.com>
2025-05-01 12:39:22 +0530

    modified a Load logic

--- src/Questions/QuestionsMain.jsx
+++ src/Questions/QuestionsMain.jsx
@@ -125,7 +125,7 @@ export default function QuestionsMain() {
 
             if (res.ok && data.status === 'success') {
                 console.log('Script output:', data.output);  // Use `data.output` based on backend structure
-                setRedirectCountdown(0)
+                window.location.href = `/workspace/${id}/${selectedFramework}`;
               } else {
                 console.warn('Backend responded with error:', data);
                 alert('Script failed to start the assessment.');
