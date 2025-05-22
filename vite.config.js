ca0f6cec46a59f107bd2cb123702b6df94c80e81
Sankar K G <sankarfrompalani@gmail.com>
2025-05-22 11:36:59 +0530

    fixed timer issues

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,5 @@ export default defineConfig({
   server:{
     host:true,
     port:5194,
-  },
-  proxy: {
-    '/api': {
-      target: 'http://192.168.253.134:5001',
-      changeOrigin: true,
-    },
-  },
+  }
 })
